b60732e499e4c3d6b576ff5ebc772080ee36e140
Gabriel Chertok <gchertok@gmail.com>
2015-09-29 08:19:49 -0300

    changing bower package to At.js

--- blueprints/ember-cli-at-js/index.js
+++ blueprints/ember-cli-at-js/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   beforeInstall: function() {
-    return this.addBowerPackageToProject('jquery.atwho');
+    return this.addBowerPackageToProject('At.js');
   }
 };
